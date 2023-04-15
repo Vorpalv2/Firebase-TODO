1db6035d89e012705c32b57cfd04d3cfe90bc51d
Aakash <aakashanand1995@gmail.com>
2023-04-15 09:33:54 +0530

    module added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref ,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const AppSetting ={ databaseURL:"https://playground-507f5-default-rtdb.asia-southeast1.firebasedatabase.app/"}
+
+const app = initializeApp(AppSetting);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
+
 addButtonEl.addEventListener("click",function(){
     let inputValue = inputFieldEl.value;
+    push(shoppingListInDB,inputValue);
     console.log(inputValue);
 });
