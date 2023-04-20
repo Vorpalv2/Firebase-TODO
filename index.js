b76f31e5da16f228ffb451b068ee08b7d1dfca9a
Aakash <aakashanand1995@gmail.com>
2023-04-20 20:33:01 +0530

    more updates

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const AppSetting ={ databaseURL:"https://playground-507f5-default-rtdb.asia-sout
 const app = initializeApp(AppSetting);
 const database = getDatabase(app);
 const shoppingListInDB = ref(database, "shoppingList");
+
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
-
+const listItem = document.getElementById("list");
 
 addButtonEl.addEventListener("click",function(){
     let inputValue = inputFieldEl.value;
-    push(shoppingListInDB,inputValue);
-    console.log(inputValue);
+    console.log(typeof inputValue);
+    let toupper = inputValue.toUpperCase();
+    console.log(toupper);
+    push(shoppingListInDB,toupper);
+    console.log(toupper);
+
+
+    listItem.innerHTML += `<li>${toupper}</li>`;
+
 });
